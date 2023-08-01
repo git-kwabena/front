064f6791ccb455d3949afee2b65c86037ac7baf8
Kwabena Afoakwah <82213869+github-kwabena@users.noreply.github.com>
2023-08-01 11:51:50 -0400

    up

--- app/lib/mongodb.js
+++ app/lib/mongodb.js
@@ -2,10 +2,10 @@ import mongoose from "mongoose"
 
 const connectDB = async () => {
     try {
-        if (mongoose.connection.readyState === 0){
+        //if (mongoose.connection.readyState === 0){
             await mongoose.connect(process.env.NEXT_PUBLIC_MONGODB_URI)
             console.log("db connected")
-        }
+       // }
     } catch(error){
         console.log(error)
 
